d01a2964291f164bd713f7e4cd2bcca177e3cc51
xNikhil0901 <nikhiln@primethic.com>
2024-04-17 17:36:14 +0530

    changed seeding according too new table design

--- priv/repo/migrations/20240415052133_add_synced_tables_data.exs
+++ priv/repo/migrations/20240415052133_add_synced_tables_data.exs
@@ -2,9 +2,6 @@ defmodule Syncapp.Repo.Migrations.AddSyncedTablesData do
   use Ecto.Migration
 
   @tables_to_sync ~w(test cities)s
-  @time_in_hours 24
-  @time Application.fetch_env!(:syncapp, :startup_time)
-  @users [1, 4, 67, 89]
 
   def up do
     seed()
@@ -19,15 +16,11 @@ defmodule Syncapp.Repo.Migrations.AddSyncedTablesData do
   end
 
   defp default_data do
-    Enum.flat_map(@users, fn user ->
-      Enum.map(@tables_to_sync, fn table ->
-        %{
-          table_name: table,
-          table_fields: build_data(table),
-          last_synced_datetime: @time,
-          user_id: user
-        }
-      end)
+    Enum.map(@tables_to_sync, fn table ->
+      %{
+        table_name: table,
+        table_fields: build_data(table)
+      }
     end)
   end
 
@@ -35,9 +28,5 @@ defmodule Syncapp.Repo.Migrations.AddSyncedTablesData do
     str = Syncapp.Tables.Info.get_columns(table) |> Enum.join(" ")
 
     "~w(" <> str <> ")a"
-  end
-
-  defp get_datetime do
-    NaiveDateTime.local_now() |> NaiveDateTime.add(-@time_in_hours, :hour)
   end
 end
