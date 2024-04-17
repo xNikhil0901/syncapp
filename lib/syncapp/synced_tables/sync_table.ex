3c710a02bf3dd3a45daf7a216d97d47ea3991e97
xNikhil0901 <nikhiln@primethic.com>
2024-04-17 17:34:17 +0530

    New schema for users_sync_table

--- lib/syncapp/synced_tables/sync_table.ex
+++ lib/syncapp/synced_tables/sync_table.ex
@@ -2,17 +2,21 @@ defmodule Syncapp.SyncedTables.SyncTable do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:sync_table_id, :id, autogenerate: true}
   schema "synced_tables" do
     field :table_name, :string
     field :table_fields, :string
-    field :user_id, :integer
-    field :last_synced_datetime, :naive_datetime
+
+    has_many(:user_sync_table, Syncapp.UsersSyncTable.UserSyncTable,
+      foreign_key: :sync_table_id,
+      references: :sync_table_id
+    )
   end
 
   @doc false
   def changeset(sync_table, attrs) do
     sync_table
-    |> cast(attrs, [:table_name, :table_fields, :user_id, :last_synced_datetime])
+    |> cast(attrs, [:table_name, :table_fields])
     |> validate_required([:table_name])
   end
 end
