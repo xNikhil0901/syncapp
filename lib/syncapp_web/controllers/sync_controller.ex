741b6125099f8de14635ba83adfcdf01406a99c7
xNikhil0901 <nikhiln@primethic.com>
2024-04-17 17:35:29 +0530

    added chunk based response

--- lib/syncapp_web/controllers/sync_controller.ex
+++ lib/syncapp_web/controllers/sync_controller.ex
@@ -2,6 +2,8 @@ defmodule SyncappWeb.SyncController do
   use SyncappWeb, :controller
 
   action_fallback SyncappWeb.FallbackController
+  @allowed_content_length 1000
+  @size 100
 
   def get_unsynced_data(conn, params) do
     result =
@@ -10,16 +12,30 @@ defmodule SyncappWeb.SyncController do
         String.to_integer(params["user_id"])
       )
 
-    Syncapp.SyncedTables.update_sync_table(params)
+    Syncapp.UsersSyncTable.update_user_sync_table(params["table_name"], params["user_id"])
 
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(
-      200,
-      Jason.encode!(%{status: "success", data: result})
-    )
+    if length(result) > @allowed_content_length do
+      send_data_in_chunk(conn, result)
+    else
+      json(conn, %{status: "success", data: result})
+    end
   end
 
+  defp send_data_in_chunk(conn, result) do
+    conn = conn |> put_resp_content_type("application/json") |> send_chunked(200)
+
+    Enum.reduce_while(Enum.chunk_every(result, @size), conn, fn chunk, conn ->
+      case chunk(conn, Jason.encode!(chunk)) do
+        {:ok, conn} ->
+          {:cont, conn}
+
+        {:error, :closed} ->
+          {:halt, conn}
+      end
+    end)
+  end
+
+  @spec sync_server(Plug.Conn.t(), nil | maybe_improper_list() | map()) :: Plug.Conn.t()
   def sync_server(conn, params) do
     Syncapp.SyncedTables.upsert_sync_tables(params["update_synced_tables"])
 
