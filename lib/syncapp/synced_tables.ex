f56b4ec45955d25144e1bd2274a7cd34ddf83456
xNikhil0901 <nikhiln@primethic.com>
2024-04-18 16:56:26 +0530

    removed unwanted code

--- lib/syncapp/synced_tables.ex
+++ lib/syncapp/synced_tables.ex
@@ -10,12 +10,6 @@ defmodule Syncapp.SyncedTables do
 
   @doc """
   Returns the list of synced_tables.
-
-  ## Examples
-
-      iex> list_synced_tables()
-      [%SyncTable{}, ...]
-
   """
   def list_synced_tables do
     Repo.all(SyncTable)
@@ -30,15 +24,6 @@ defmodule Syncapp.SyncedTables do
 
   @doc """
   Creates a sync_table.
-
-  ## Examples
-
-      iex> create_sync_table(%{field: value})
-      {:ok, %SyncTable{}}
-
-      iex> create_sync_table(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_sync_table(attrs \\ %{}) do
     %SyncTable{}
@@ -46,41 +31,8 @@ defmodule Syncapp.SyncedTables do
     |> Repo.insert()
   end
 
-  def upsert_sync_tables(_attrs \\ [[]]) do
-    data =
-      [
-        %{"table_name" => "users", "last_synced_datetime" => NaiveDateTime.local_now()},
-        %{"table_name" => "employers", "last_synced_datetime" => NaiveDateTime.local_now()}
-      ]
-      |> transform_data()
-
-    Repo.insert_all(SyncTable, data,
-      on_conflict: {:replace, [:last_synced_datetime]},
-      conflict_target: :table_name
-    )
-  end
-
-  defp transform_data(data) do
-    Enum.map(data, fn x ->
-      Enum.map(x, fn {key, val} ->
-        if key == "last_synced_datetime",
-          do: {String.to_atom(key), NaiveDateTime.from_iso8601!(val)},
-          else: {String.to_atom(key), val}
-      end)
-    end)
-  end
-
   @doc """
   Updates a sync_table.
-
-  ## Examples
-
-      iex> update_sync_table(sync_table, %{field: new_value})
-      {:ok, %SyncTable{}}
-
-      iex> update_sync_table(sync_table, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_sync_table(attrs) do
     result =
